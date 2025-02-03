589829f5ab23bc630b0e63d175453f8d88616d86
Alex Petrov <alexpetrov@yourdomain.example.com>
2025-02-03 18:48:52 +0300

    add print_module functions in modules 2, 3

--- module2.py
+++ module2.py
@@ -5,3 +5,7 @@ from datetime import datetime
 def print_hello_and_date():
     now = datetime.now()   
     print(f"{now}: hello")
+
+
+def print_module2():
+    print("hello from module 2")
---
+++ module3.py
@@ -0,0 +1,3 @@
+def print_module3():
+    print("hello from module 3")
+
