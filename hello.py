703163e3742b11596faf8292b1a201a36beb4a94
Alex Petrov <alexpetrov@yourdomain.example.com>
2025-02-03 18:06:03 +0300

    hello

---
+++ hello.py
@@ -0,0 +1,4 @@
+from module2 import *
+
+print("hello")
+print_hello_and_date()
---
+++ module2.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+
+
+def print_hello_and_date():
+    now = datetime.now()   
+    print(f"{now}: hello")
