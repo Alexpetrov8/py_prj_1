2a5d4a2a8bba5938bc8b95e93f56d24dd7f598d6
Alex Petrov <alexpetrov@yourdomain.example.com>
2025-02-04 11:43:57 +0300

    add module 5

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 from module2 import *
 from module3 import *
 from module4 import *
+from module5 import *
 
 from datetime import datetime
 
@@ -10,3 +11,4 @@ print(f"{now}:  hello")
 print_module2()
 print_module3()
 print_module4()
+print_module5()
---
+++ module5.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+
+def print_module5():
+    now = datetime.now()   
+    print(f"{now}:  hello from module 5")
+
