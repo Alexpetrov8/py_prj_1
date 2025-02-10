5155323d5d8fd2c6b490c33e556107badc924388
Alex Petrov <alexpetrov@yourdomain.example.com>
2025-02-10 14:31:53 +0300

    module 5 second msg

--- module5.py
+++ module5.py
@@ -3,4 +3,5 @@ from datetime import datetime
 def print_module5():
     now = datetime.now()   
     print(f"{now}:  hello from module 5")
+    print(f"{now}:  hello from module 5 second message")
 
