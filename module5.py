46d50007783f536be3201b223df30ec620dccfe7
Alex Petrov <alexpetrov@yourdomain.example.com>
2025-02-23 21:56:59 +0300

    add errors msg to modue 5

--- module5.py
+++ module5.py
@@ -5,4 +5,6 @@ def print_module5():
     print(f"{now}:  hello from module 5")
     print(f"{now}:  hello from module 5 second message")
     print(f"{now}:  hello from module 5 third message")
+    print(f"{now}:  ERROR 1 from module 5")
+    print(f"{now}:  ERROR 2 from module 5")
 
