c5a139d588f9a738afc2a1b263d8d1967fe70c6c
Alex Petrov <alexpetrov@yourdomain.example.com>
2025-03-08 13:23:05 +0300

    add user auth func module and modyfi hello

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ from module2 import *
 from module3 import *
 from module4 import *
 from module5 import *
+from user_auth import *
 
 from datetime import datetime
 
@@ -12,3 +13,4 @@ print_module2()
 print_module3()
 print_module4()
 print_module5()
+user_auth("user1", "87n9m9n96b^BJ")
---
+++ user_auth.py
@@ -0,0 +1,6 @@
+
+
+
+def user_auth(u_login, u_pass):
+    print(f"User Auth: login: {u_login}, pass: {u_pass}")
+    return 0
